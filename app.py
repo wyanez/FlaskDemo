f5cef358521c8b73fb72e2dfd76e3fafeb2c0cb4
William Yanez <wyanez@gmail.com>
2025-05-22 23:55:41 -0400

    feat: added endpoint datetime and its tests

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from datetime import UTC, datetime
 
 app = Flask(__name__)
 
@@ -10,6 +11,17 @@ def home():
 def about():
     return jsonify({"version": "1.0", "author": "William Yanez"}), 200
 
+@app.route('/datetime', defaults={'format': 'local'})
+@app.route('/datetime/<string:format>')
+def current_datetime(format):
+    """return the current date and time in the specified format"""
+    if format.lower() == "utc":
+        now = datetime.now(UTC).isoformat()
+    else:
+        now = datetime.now().isoformat()
+    
+    return jsonify({"datetime": now, "format": format}), 200
+
 if __name__ == "__main__":
     app.run(debug=True)
 
@@ -17,6 +29,7 @@ if __name__ == "__main__":
 # This is a simple Flask API with two endpoints: /
 # 1. The root endpoint ("/") returns a welcome message.
 # 2. The "/about" endpoint returns version and author information.
+# 3. The "/datetime" endpoint returns the current date and time in either UTC or local format.
 # To run the API, use the command: python app.py
 # The API will be accessible at http://127.0.0.1:5000/
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pytest
 from ..app import app
 
@@ -6,6 +7,12 @@ def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+
+def _validate_isoformat(json_data):
+    try:
+        datetime.fromisoformat(json_data["datetime"])
+    except ValueError:
+        assert False, "Invalid date format"
 
 def test_home(client):
     response = client.get('/')
@@ -16,3 +23,28 @@ def test_about(client):
     response = client.get('/about')
     assert response.status_code == 200
     assert response.get_json() == {"version": "1.0", "author": "William Yanez"}
+
+def test_datetime_local(client):
+    """Verify that the '/datetime' endpoint returns the local time by default."""
+    response = client.get('/datetime')
+
+    assert response.status_code == 200
+    json_data = response.get_json()
+
+    assert "datetime" in json_data
+    assert json_data["format"] == "local"
+
+    _validate_isoformat(json_data)
+
+
+def test_datetime_utc(client):
+    """Verify that the '/datetime/utc' endpoint returns the UTC time."""
+    response = client.get('/datetime/utc')
+    
+    assert response.status_code == 200
+    json_data = response.get_json()
+
+    assert "datetime" in json_data
+    assert json_data["format"] == "utc"
+
+    _validate_isoformat(json_data)
