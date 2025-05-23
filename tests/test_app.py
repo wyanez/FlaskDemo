6e9dcfff6c236f5b755d426fe0c53f9009e4215c
William Yanez <wyanez@gmail.com>
2025-05-22 23:25:55 -0400

    test: implement test for each endpoint. add gitignore

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+import pytest
+from ..app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Welcome to the Flask API!"}
+
+def test_about(client):
+    response = client.get('/about')
+    assert response.status_code == 200
+    assert response.get_json() == {"version": "1.0", "author": "William Yanez"}
